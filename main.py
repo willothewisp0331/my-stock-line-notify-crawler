80a51dc97841148c2418d8fa3be8c429c716864f
nienpingchen <willothewisp890331@gmail.com>
2025-05-22 23:59:37 +0800

    改成flex message

--- main.py
+++ main.py
@@ -6,63 +6,341 @@ from io import StringIO
 import os
 import json
 from linebot.v3.messaging import MessagingApi, Configuration, ApiClient, PushMessageRequest
-from linebot.v3.messaging.models import TextMessage
+from linebot.v3.messaging.models import FlexMessage, TextMessage, Message
 import re
+from dotenv import load_dotenv
 
 DATA_FILE = 'daily_open_interest.json'
-user_lin = "U2e7ddaa0d777d91a3496d7336da8edec"
 
-def send_line_message(message_text):
-    access_token = os.environ.get("LINE_TOKEN")
-    line_user_id = os.environ.get("USER_ID")
+def send_line_message(flex_msg):
+        try:
+            access_token = os.environ.get("LINE_TOKEN")
+            line_user_id = [os.environ.get("USER_ID1")]
+            if not access_token:
+                print("環境變數未設定，請確認 bat 檔或 .env 檔是否正確！")
+            configuration = Configuration(access_token=access_token)
+            with ApiClient(configuration) as api_client:
+                for id in line_user_id:
+                    MessagingApi(api_client).push_message(
+                        PushMessageRequest(to=id, messages=[flex_msg])
+                    )
+            print("已成功傳送Line訊息")
+        except Exception as e:
+            print(f"[錯誤] 發送 LINE 訊息失敗：{e}")
 
-    configuration = Configuration(access_token=access_token)
-    with ApiClient(configuration) as api_client:
-        messaging_api = MessagingApi(api_client)
-        messaging_api.push_message(
-            PushMessageRequest(
-                to=line_user_id,
-                messages=[TextMessage(text=message_text)]
-            )
-        )
+def generate_flex_message_dict(today_data, yesterday_data):
+    def format_diff(current, prev):
+        diff = current - prev
+        sign = "+" if diff >= 0 else ""
+        return f"({sign}{diff:,}）"
+    if today_data['date'] == yesterday_data['date']:
+        return "[錯誤] 日期與昨天相同，無法比較"
+    else:
+        data1 = f"{today_data['taiex']:,}"
+        data2 = f"{today_data['fut_total']:,}"
+        data3 = f"{today_data['opt_taiex']:,}"
+        data4 = f"{today_data['opt_total']:,}"
+        data5 = f"{today_data['large_5']:,}"
+        data6 = f"{today_data['large_10']:,}"
+        bubble_dict = {
+          "type": "bubble",
+          "body": {
+            "type": "box",
+            "layout": "vertical",
+            "spacing": "md",
+            "contents": [
+              {
+                "type": "text",
+                "text": today_data['date'],
+                "weight": "bold",
+                "size": "lg",
+                "color": "#0B6E99"
+              },
+              {
+                "type": "box",
+                "layout": "horizontal",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "類型",
+                    "size": "md",
+                    "color": "#555555",
+                    "flex": 4
+                  },
+                  {
+                    "type": "text",
+                    "text": "未平倉口數",
+                    "size": "xs",
+                    "color": "#555555",
+                    "flex": 3
+                  },
+                  {
+                    "type": "text",
+                    "text": "與前值相比",
+                    "size": "xs",
+                    "color": "#555555",
+                    "flex": 0
+                  }
+                ]
+              },
+              {
+                "type": "separator",
+                "margin": "md"
+              },
+              {
+                "type": "box",
+                "layout": "vertical",
+                "margin": "md",
+                "spacing": "sm",
+                "contents": [
+                  {
+                    "type": "box",
+                    "layout": "horizontal",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": "外資台指期貨",
+                        "size": "sm",
+                        "color": "#555555",
+                        "flex": 3
+                      },
+                      {
+                        "type": "text",
+                        "text": data1,
+                        "size": "sm",
+                        "color": "#111111",
+                        "flex": 2,
+                        "align": "end"
+                      },
+                      {
+                        "type": "text",
+                        "text": format_diff(today_data['taiex'], yesterday_data['taiex']),
+                        "size": "sm",
+                        "color": "#FF5555",
+                        "flex": 2,
+                        "align": "end"
+                      }
+                    ]
+                  },
+                  {
+                    "type": "box",
+                    "layout": "horizontal",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": "外資期貨總計",
+                        "size": "sm",
+                        "color": "#555555",
+                        "flex": 3
+                      },
+                      {
+                        "type": "text",
+                        "text": data2,
+                        "size": "sm",
+                        "color": "#111111",
+                        "flex": 2,
+                        "align": "end"
+                      },
+                      {
+                        "type": "text",
+                        "text": format_diff(today_data['fut_total'], yesterday_data['fut_total']),
+                        "size": "sm",
+                        "color": "#FF5555",
+                        "flex": 2,
+                        "align": "end"
+                      }
+                    ]
+                  },
+                  {
+                    "type": "separator"
+                  },
+                  {
+                    "type": "box",
+                    "layout": "horizontal",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": "外資選擇權",
+                        "size": "sm",
+                        "color": "#555555",
+                        "flex": 3
+                      },
+                      {
+                        "type": "text",
+                        "text": data3,
+                        "size": "sm",
+                        "color": "#111111",
+                        "flex": 2,
+                        "align": "end"
+                      },
+                      {
+                        "type": "text",
+                        "text": format_diff(today_data['opt_taiex'], today_data['opt_taiex']),
+                        "size": "sm",
+                        "color": "#00AA00",
+                        "flex": 2,
+                        "align": "end"
+                      }
+                    ]
+                  },
+                  {
+                    "type": "box",
+                    "layout": "horizontal",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": "外資選擇權總計",
+                        "size": "sm",
+                        "color": "#555555",
+                        "flex": 3
+                      },
+                      {
+                        "type": "text",
+                        "text": data4,
+                        "size": "sm",
+                        "color": "#111111",
+                        "flex": 2,
+                        "align": "end"
+                      },
+                      {
+                        "type": "text",
+                        "text": format_diff(today_data['opt_total'], yesterday_data['opt_total']),
+                        "size": "sm",
+                        "color": "#00AA00",
+                        "flex": 2,
+                        "align": "end"
+                      }
+                    ]
+                  },
+                  {
+                    "type": "separator"
+                  },
+                  {
+                    "type": "box",
+                    "layout": "horizontal",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": "前五大台指期",
+                        "size": "sm",
+                        "color": "#555555",
+                        "flex": 3
+                      },
+                      {
+                        "type": "text",
+                        "text": data5,
+                        "size": "sm",
+                        "color": "#111111",
+                        "flex": 2,
+                        "align": "end"
+                      },
+                      {
+                        "type": "text",
+                        "text": format_diff(today_data['large_5'], yesterday_data['large_5']),
+                        "size": "sm",
+                        "color": "#FF5555",
+                        "flex": 2,
+                        "align": "end"
+                      }
+                    ]
+                  },
+                  {
+                    "type": "box",
+                    "layout": "horizontal",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": "前十大台指期",
+                        "size": "sm",
+                        "color": "#555555",
+                        "flex": 3
+                      },
+                      {
+                        "type": "text",
+                        "text": data6,
+                        "size": "sm",
+                        "color": "#111111",
+                        "flex": 2,
+                        "align": "end"
+                      },
+                      {
+                        "type": "text",
+                        "text": format_diff(today_data['large_10'], yesterday_data['large_10']),
+                        "size": "sm",
+                        "color": "#FF5555",
+                        "flex": 2,
+                        "align": "end"
+                      }
+                    ]
+                  },
+                  {
+                    "type": "separator"
+                  }
+                ]
+              }
+            ]
+          }
+        }
+        print(type(bubble_dict))
+        flex_msg = FlexMessage.from_dict({
+            "type": "flex",
+            "altText": "股市資訊",
+            "contents": bubble_dict
+        })
+    return flex_msg
+
 
 def fetch_taiex_futures_data(query_date):
-    url = "https://www.taifex.com.tw/cht/3/futContractsDate"
-    http = urllib3.PoolManager()
-    response = http.request(
-        'POST',
-        url,
-        fields={
-            'queryType': 1,
-            'doQuery': 1,
-            'queryDate': query_date
-        }
-    )
-    soup = BeautifulSoup(response.data, 'html.parser')
-    table_html = soup.find_all('table')
-    data_frame = pd.read_html(StringIO(str(table_html)))[0]
-    foreign_total_open_interest = int(data_frame.iloc[71, 13])
-    taiex_open_interest = int(data_frame.iloc[2, 13])
-    return taiex_open_interest, foreign_total_open_interest
+    try:
+        url = "https://www.taifex.com.tw/cht/3/futContractsDate"
+        http = urllib3.PoolManager()
+        response = http.request(
+            'POST',
+            url,
+            fields={
+                'queryType': 1,
+                'doQuery': 1,
+                'queryDate': query_date
+            }
+        )
+        soup = BeautifulSoup(response.data, 'html.parser')
+        table_html = soup.find_all('table')
+        data_frame = pd.read_html(StringIO(str(table_html)))[0]
+        for i in range(len(data_frame)):
+            if data_frame.iloc[i, 1] == "期貨 小計" and data_frame.iloc[i, 2] == "外資":
+                foreign_total_open_interest = int(data_frame.iloc[i, 13])
+            if data_frame.iloc[i, 1] == "臺股期貨" and data_frame.iloc[i, 2] == "外資":
+                taiex_open_interest = int(data_frame.iloc[i, 13])
+        return taiex_open_interest, foreign_total_open_interest
+    except Exception as e:
+        print(f"[錯誤] 抓取資料失敗：{e}")
+        return None
 
 def fetch_option_data(query_date):
-    url = "https://www.taifex.com.tw/cht/3/optContractsDate"
-    http = urllib3.PoolManager()
-    response = http.request(
-        'POST',
-        url,
-        fields={
-            'queryType': 1,
-            'doQuery': 1,
-            'queryDate': query_date
-        }
-    )
-    soup = BeautifulSoup(response.data, 'html.parser')
-    table_html = soup.find_all('table')
-    data_frame = pd.read_html(StringIO(str(table_html)))[0]
-    foreign_total_option_open_interest = int(data_frame.iloc[17, 13])
-    option_open_interest = int(data_frame.iloc[2, 13])
-    return option_open_interest, foreign_total_option_open_interest
+    try:
+        url = "https://www.taifex.com.tw/cht/3/optContractsDate"
+        http = urllib3.PoolManager()
+        response = http.request(
+            'POST',
+            url,
+            fields={
+                'queryType': 1,
+                'doQuery': 1,
+                'queryDate': query_date
+            }
+        )
+        soup = BeautifulSoup(response.data, 'html.parser')
+        table_html = soup.find_all('table')
+        data_frame = pd.read_html(StringIO(str(table_html)))[0]
+        for i in range(len(data_frame)):
+            if data_frame.iloc[i, 1] == "選擇權 小計" and data_frame.iloc[i, 2] == "外資":
+                foreign_total_option_open_interest = int(data_frame.iloc[i, 13])
+            if data_frame.iloc[i, 1] == "臺指選擇權" and data_frame.iloc[i, 2] == "外資":
+                option_open_interest = int(data_frame.iloc[i, 13])
+        return option_open_interest, foreign_total_option_open_interest
+    except Exception as e:
+        print(f"[錯誤] 抓取資料失敗：{e}")
+        return None
 
 def clean_data_to_int(data):
     data_clean = re.sub(r"\s*\(.*?\)", "", data)
@@ -70,53 +348,69 @@ def clean_data_to_int(data):
     return int(data_clean)
 
 def fetch_large_future_data(query_date):
-    url = "https://www.taifex.com.tw/cht/3/largeTraderFutQry"
-    http = urllib3.PoolManager()
-    response = http.request(
-        'POST',
-        url,
-        fields={
-            'contractId': 'TX',
-            'queryDate': query_date
-        }
-    )
-    soup = BeautifulSoup(response.data, 'html.parser')
-    table_html = soup.find_all('table')
-    data_frame = pd.read_html(StringIO(str(table_html)))[0]
-    large_open_interest = clean_data_to_int(data_frame.iloc[1, 2]) - clean_data_to_int(data_frame.iloc[1, 6])
-    large_total_open_interest = clean_data_to_int(data_frame.iloc[1, 4]) - clean_data_to_int(data_frame.iloc[1, 8])
-    return large_open_interest, large_total_open_interest
+    try:
+        url = "https://www.taifex.com.tw/cht/3/largeTraderFutQry"
+        http = urllib3.PoolManager()
+        response = http.request(
+            'POST',
+            url,
+            fields={
+                'contractId': 'TX',
+                'queryDate': query_date
+            }
+        )
+        soup = BeautifulSoup(response.data, 'html.parser')
+        table_html = soup.find_all('table')
+        data_frame = pd.read_html(StringIO(str(table_html)))[0]
+        large_open_interest = clean_data_to_int(data_frame.iloc[1, 2]) - clean_data_to_int(data_frame.iloc[1, 6])
+        large_total_open_interest = clean_data_to_int(data_frame.iloc[1, 4]) - clean_data_to_int(data_frame.iloc[1, 8])
+        return large_open_interest, large_total_open_interest
+    except Exception as e:
+        print(f"[錯誤] 抓取資料失敗：{e}")
+        return None
 
 def load_previous_data():
-    if not os.path.exists(DATA_FILE):
+    try:
+        if not os.path.exists(DATA_FILE):
+            return None
+        with open(DATA_FILE, 'r') as f:
+            return json.load(f)
+    except Exception as e:
+        print(f"[錯誤] 載入昨天資料失敗：{e}")
         return None
-    with open(DATA_FILE, 'r') as f:
-        return json.load(f)
 
 def save_today_data(data):
-    with open(DATA_FILE, 'w') as f:
-        json.dump(data, f)
+    try:
+        with open(DATA_FILE, 'w') as f:
+            json.dump(data, f)
+    except Exception as e:
+        print(f"[錯誤] 儲存今天資料失敗：{e}")
 
-
-def generate_comparison(today_data, yesterday_data):
-    def format_diff(current, prev):
-        diff = current - prev
-        sign = "+" if diff >= 0 else ""
-        return f"{current:,}（{sign}{diff:,}）"
-
-    return (
-        f"{today_data['date']}\n"
-        f"外資大台期貨未平倉口數：{format_diff(today_data['taiex'], yesterday_data['taiex'])} 口\n"
-        f"外資期貨總計未平倉口數：{format_diff(today_data['fut_total'], yesterday_data['fut_total'])} 口\n"
-        f"外資台指選擇權未平倉口數：{format_diff(today_data['opt_taiex'], yesterday_data['opt_taiex'])} 口\n"
-        f"外資選擇權總計未平倉口數：{format_diff(today_data['opt_total'], yesterday_data['opt_total'])} 口\n"
-        f"前五大台指期貨未平倉口數：{format_diff(today_data['large_5'], yesterday_data['large_5'])} 口\n"
-        f"前十大台指期貨未平倉口數：{format_diff(today_data['large_10'], yesterday_data['large_10'])} 口"
-    )
+#
+# def generate_comparison(today_data, yesterday_data):
+#     def format_diff(current, prev):
+#         diff = current - prev
+#         sign = "+" if diff >= 0 else ""
+#         return f"{current:,}（{sign}{diff:,}）"
+#     if today_data['date'] == yesterday_data['date']:
+#         return "[錯誤] 日期與昨天相同，無法比較"
+#     else:
+#         return (
+#             f"{today_data['date']}\n"
+#             f"外資大台期貨未平倉口數：{format_diff(today_data['taiex'], yesterday_data['taiex'])} 口\n"
+#             f"外資期貨總計未平倉口數：{format_diff(today_data['fut_total'], yesterday_data['fut_total'])} 口\n"
+#             f"外資台指選擇權未平倉口數：{format_diff(today_data['opt_taiex'], yesterday_data['opt_taiex'])} 口\n"
+#             f"外資選擇權總計未平倉口數：{format_diff(today_data['opt_total'], yesterday_data['opt_total'])} 口\n"
+#             f"前五大台指期貨未平倉口數：{format_diff(today_data['large_5'], yesterday_data['large_5'])} 口\n"
+#             f"前十大台指期貨未平倉口數：{format_diff(today_data['large_10'], yesterday_data['large_10'])} 口"
+#         )
 
 if __name__ == '__main__':
-    # today_str = date.today().strftime('%Y/%m/%d')
-    today_str = "2025/05/16"
+    # 載入環境變數
+    load_dotenv()
+
+    # 取得今天日期
+    today_str = date.today().strftime('%Y/%m/%d')
     taiex_oi, fut_total_oi = fetch_taiex_futures_data(today_str)
     opt_oi, opt_total_oi = fetch_option_data(today_str)
     large_5_oi, large_10_oi = fetch_large_future_data(today_str)
@@ -134,22 +428,8 @@ if __name__ == '__main__':
     # 載入昨天的資料
     yesterday_data = load_previous_data()
 
-    # 有昨天資料就做比較，沒有就顯示今天的數據
-    if yesterday_data:
-        message_text = generate_comparison(today_data, yesterday_data)
-    else:
-        message_text = (
-            f"{today_data['date']}\n"
-            f"外資台指期貨未平倉口數：{today_data['taiex']:,} 口\n"
-            f"外資期貨總計未平倉口數：{today_data['fut_total']:,} 口\n"
-            f"外資台指選擇權未平倉口數：{today_data['opt_taiex']:,} 口\n"
-            f"外資選擇權總計未平倉口數：{today_data['opt_total']:,} 口\n"
-            f"前五大台指期貨未平倉口數：{today_data['large_5']:,} 口\n"
-            f"前十大台指期貨未平倉口數：{today_data['large_10']:,} 口\n"
-        )
-
-    print(message_text)
-    # send_line_message(message_text)  # 你要開就取消註解
+    flex_message = generate_flex_message_dict(today_data, yesterday_data)
+    send_line_message(flex_message)
 
     # 儲存今天資料，給明天比對用
     save_today_data(today_data)
--- test.py
+++ test.py
@@ -1,35 +1,197 @@
-# app.py
-from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.v3.messaging import MessagingApi, Configuration, ApiClient, PushMessageRequest
+from linebot.v3.messaging.models.flex_message import FlexMessage
+import os
 
-app = Flask(__name__)
+access_token = os.environ.get("LINE_TOKEN")
+user_id = os.environ.get("USER_ID1")
+print(access_token)
+print(user_id)
 
-line_bot_api = LineBotApi('aHmpfQGEB7ncEBBPbaFBtZ6rbljmwXgByvCf5/fXIT2eVzsvZ59VhQNpzqfvQpjieram9XSkJU66xTiOrHL6NsdNsDEgeVnNR5ZK2lGmRw4L8Wan3yZQZDUCTgVaOJkJ/9OBXO9E3ROmD5oSdyShzwdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('3c3470405b469a2ebdaf5ceb1547c327')
+# 組成 Flex bubble dict
+bubble_dict = {
+  "type": "bubble",
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "股市資訊",
+        "weight": "bold",
+        "color": "#1DB446",
+        "size": "sm"
+      },
+      {
+        "type": "separator",
+        "margin": "sm"
+      },
+      {
+        "type": "box",
+        "layout": "vertical",
+        "margin": "xxl",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+              {
+                "type": "text",
+                "text": "台指期貨",
+                "size": "sm",
+                "color": "#555555",
+                "flex": 0
+              },
+              {
+                "type": "text",
+                "text": "-40656口",
+                "size": "sm",
+                "color": "#111111",
+                "align": "end"
+              }
+            ]
+          },
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+              {
+                "type": "text",
+                "text": "期貨商品總計",
+                "size": "sm",
+                "color": "#555555",
+                "flex": 0
+              },
+              {
+                "type": "text",
+                "text": "-313639口",
+                "size": "sm",
+                "color": "#111111",
+                "align": "end"
+              }
+            ]
+          },
+          {
+            "type": "box",
+            "layout": "vertical",
+            "margin": "xxl",
+            "spacing": "sm",
+            "contents": [
+              {
+                "type": "box",
+                "layout": "horizontal",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "台指期貨",
+                    "size": "sm",
+                    "color": "#555555",
+                    "flex": 0
+                  },
+                  {
+                    "type": "text",
+                    "text": "-40656口",
+                    "size": "sm",
+                    "color": "#111111",
+                    "align": "end"
+                  }
+                ]
+              },
+              {
+                "type": "box",
+                "layout": "horizontal",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "期貨商品總計",
+                    "size": "sm",
+                    "color": "#555555",
+                    "flex": 0
+                  },
+                  {
+                    "type": "text",
+                    "text": "-313639口",
+                    "size": "sm",
+                    "color": "#111111",
+                    "align": "end"
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            "type": "box",
+            "layout": "vertical",
+            "margin": "xxl",
+            "spacing": "sm",
+            "contents": [
+              {
+                "type": "box",
+                "layout": "horizontal",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "台指期貨",
+                    "size": "sm",
+                    "color": "#555555",
+                    "flex": 0
+                  },
+                  {
+                    "type": "text",
+                    "text": "-40656口",
+                    "size": "sm",
+                    "color": "#111111",
+                    "align": "end"
+                  }
+                ]
+              },
+              {
+                "type": "box",
+                "layout": "horizontal",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "期貨商品總計",
+                    "size": "sm",
+                    "color": "#555555",
+                    "flex": 0
+                  },
+                  {
+                    "type": "text",
+                    "text": "-313639口",
+                    "size": "sm",
+                    "color": "#111111",
+                    "align": "end"
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "type": "separator",
+        "margin": "xxl"
+      }
+    ]
+  },
+  "styles": {
+    "footer": {
+      "separator": True
+    }
+  }
+}
 
-@app.route("/callback", methods=['POST'])
-def callback():
-    # 確認 Signature
-    signature = request.headers['X-Line-Signature']
-    body = request.get_data(as_text=True)
+# 用 FlexMessage.from_dict 包起來
+flex_msg = FlexMessage.from_dict({
+    "type": "flex",
+    "altText": "股市資訊",
+    "contents": bubble_dict
+})
 
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
-
-# 處理收到的訊息
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    # 回傳一樣的文字（Echo bot）
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text)
+# 發送
+configuration = Configuration(access_token=access_token)
+with ApiClient(configuration) as api_client:
+    MessagingApi(api_client).push_message(
+        PushMessageRequest(to=user_id, messages=[flex_msg])
     )
-
-if __name__ == "__main__":
-    app.run(port=8000)
